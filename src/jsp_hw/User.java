c2268900f4ab941a90234bde744bd558677f7543
HOMENOTEB_WONPC1 <HOMENOTEB_WONPC1@NOTEBOOK-WON1779>
2020-04-26 22:43:59 +0900

    4.26 유저DB 출력

---
+++ src/jsp_hw/User.java
@@ -0,0 +1,158 @@
+package jsp_hw;
+
+public class User {
+    int id;
+    String userid;
+    String password;
+    String name;
+    String email;
+    int departmentId;
+    String departmentName;
+    boolean enabled;
+    String userType;
+
+    public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUserid() {
+		return userid;
+	}
+
+	public void setUserid(String userid) {
+		this.userid = userid;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public int getDepartmentId() {
+		return departmentId;
+	}
+
+	public void setDepartmentId(int departmentId) {
+		this.departmentId = departmentId;
+	}
+
+	public String getDepartmentName() {
+		return departmentName;
+	}
+
+	public void setDepartmentName(String departmentName) {
+		this.departmentName = departmentName;
+	}
+
+	public boolean isEnabled() {
+		return enabled;
+	}
+
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+
+	public String getUserType() {
+		return userType;
+	}
+
+	public void setUserType(String userType) {
+		this.userType = userType;
+	}
+
+	public int getid() {
+        return id;
+    }
+
+    public void setid(int id) {
+        this.id = id;
+    }
+
+    public String getuserid() {
+        return userid;
+    }
+
+    public void setuserid(String userid) {
+        this.userid = userid;
+    }
+
+    public String getpassword() {
+        return password;
+    }
+
+    public void setpassword(String password) {
+        this.password = password;
+    }
+
+    public String getname() {
+        return name;
+    }
+
+    public void setname(String name) {
+        this.name = name;
+    }
+
+    public String getemail() {
+        return email;
+    }
+
+    public void setemail(String email) {
+        this.email = email;
+    }
+
+    public int getdepartmentId() {
+        return departmentId;
+    }
+
+    public void setdepartmentId(int departmentId) {
+        this.departmentId = departmentId;
+    }
+
+    public String getdepartmentName() {
+        return departmentName;
+    }
+
+    public void setdepartmentName(String departmentName) {
+        this.departmentName = departmentName;
+    }
+
+    public boolean getenabled() {
+        return enabled;
+    }
+
+    public void setenabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public String getuserType() {
+        return userType;
+    }
+
+    public void setuserType(String userType) {
+        this.userType = userType;
+    }
+
+}
